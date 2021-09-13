8170faab9b4a0babad36b083bbb110096b4735ff
cczyWyc <274230928@qq.com>
2021-09-14 01:03:53 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# JavaTrainingCamp
+# Java进阶训练营第6期
+
+## 作业提交
+
+## week01_JVM
+    jvm进阶
+    导航
---
+++ week01_jvm/src/main/java/com/cczyWyc/trainingCamp/Test.java
@@ -0,0 +1,7 @@
+package com.cczyWyc.trainingCamp;
+
+/**
+ * @author wangyc
+ */
+public class Test {
+}
