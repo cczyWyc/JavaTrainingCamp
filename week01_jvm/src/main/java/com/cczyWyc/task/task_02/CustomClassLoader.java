3dbf30f9c9e41818762232807b555b6eb8b6fe3b
wangyc <274230928@qq.com>
2021-09-18 16:39:11 +0800

    week01_jvm:customClassLoader load Hello.xlass

--- week01_jvm/src/main/java/com/cczyWyc/task/task_02/CustomClassLoader.java
+++ week01_jvm/src/main/java/com/cczyWyc/task/task_02/CustomClassLoader.java
@@ -1,8 +1,39 @@
 package com.cczyWyc.task.task_02;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * 自定义类加载器
  * @author wangyc
  */
-public class CustomClassLoader {
+public class CustomClassLoader extends ClassLoader {
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+            InstantiationException, IllegalAccessException, InvocationTargetException {
+        Class<?> customClass = new CustomClassLoader().findClass("Hello");
+        Method customClassMethod = customClass.getMethod("hello");
+        customClassMethod.invoke(customClass.newInstance());
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        //加载xlass
+        Path path = Paths.get("week01_jvm/src/main/java/com/cczyWyc/task/task_02/Hello.xlass");
+        byte[] bytes = new byte[256];
+        try {
+            bytes = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) (255 - bytes[i]);
+        }
+        System.out.println(bytes.length);
+        return defineClass(name, bytes, 0, bytes.length);
+    }
 }
