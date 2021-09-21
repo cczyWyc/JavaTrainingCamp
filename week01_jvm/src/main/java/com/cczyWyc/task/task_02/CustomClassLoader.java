7069d530d1af52fa9beb9946b119b6769a7df5d2
cczyWyc <274230928@qq.com>
2021-09-21 15:17:00 +0800

    week01_jvm:custom ClassLoader load Hello.xlass to fix encoding

--- week01_jvm/src/main/java/com/cczyWyc/task/task_02/CustomClassLoader.java
+++ week01_jvm/src/main/java/com/cczyWyc/task/task_02/CustomClassLoader.java
@@ -8,21 +8,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * 自定义类加载器
+ * custom classloader
  * @author wangyc
  */
 public class CustomClassLoader extends ClassLoader {
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
             InstantiationException, IllegalAccessException, InvocationTargetException {
-        Class<?> customClass = new CustomClassLoader().findClass("Hello");
+        Class<?> customClass = new CustomClassLoader().loadClass("Hello");
         Method customClassMethod = customClass.getMethod("hello");
         customClassMethod.invoke(customClass.newInstance());
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        //加载xlass
+        //load xlass
         Path path = Paths.get("week01_jvm/src/main/java/com/cczyWyc/task/task_02/Hello.xlass");
         byte[] bytes = new byte[256];
         try {
