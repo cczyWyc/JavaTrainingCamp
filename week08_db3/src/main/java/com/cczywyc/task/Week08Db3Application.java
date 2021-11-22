bf903e4c39db6871ae9ba4a3b793d3528060b97b
cczyWyc <274230928@qq.com>
2021-11-23 00:53:42 +0800

    week08db3: use sharding sphere proxy,Sub-database and sub-table

---
+++ week08_db3/src/main/java/com/cczywyc/task/Week08Db3Application.java
@@ -0,0 +1,13 @@
+package com.cczywyc.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week08Db3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week08Db3Application.class, args);
+    }
+
+}
