ec5f0519a4329ae4d9432b9f6d9a45d1be1fc903
cczyWyc <274230928@qq.com>
2021-12-05 21:00:30 +0800

    week11_cache: Use jedis to achieve distributed locks

--- week11_cache/src/main/java/com/cczyWyc/redis/task08/Main.java
+++ week11_cache/src/main/java/com/cczyWyc/redis/task08/Main.java
@@ -6,4 +6,48 @@ package com.cczyWyc.redis.task08;
  * @author wangyc
  */
 public class Main {
+    /** redis lock name and value */
+    private static final String LOCK = "redis_redis";
+    /** expire time */
+    private static final int EXPIRE_TIME = 5;
+    /** goods amount */
+    private static int amount = 20;
+
+    public static void redisLock() {
+        System.out.println("redis lock test start------");
+
+        if (!RedisLock.getInstance().lock(LOCK, EXPIRE_TIME)) {
+            System.out.println(String.format("Thread %s get redis lock failed.", Thread.currentThread().getName()));
+            return;
+        }
+
+        System.out.println(String.format("Thread %s get redis lock successful.", Thread.currentThread().getName()));
+        try {
+            Thread.sleep(10000);
+            System.out.println(Thread.currentThread().getName());
+            amount -= 1;
+            System.out.println("goods reduction 1, goods number is:" + amount);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        RedisLock.getInstance().releaseLock(LOCK);
+        System.out.println("redis lock test end------");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(Main::redisLock);
+        Thread thread2 = new Thread(Main::redisLock);
+        thread1.setName("redis-test-thread1");
+        thread2.setName("redis-test-thread2");
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        Thread thread3 = new Thread(Main::redisLock);
+        thread3.start();
+        thread3.join();
+    }
 }
--- week11_cache/src/main/java/com/cczyWyc/redis/task08/RedisLock.java
+++ week11_cache/src/main/java/com/cczyWyc/redis/task08/RedisLock.java
@@ -1,6 +1,11 @@
 package com.cczyWyc.redis.task08;
 
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Collections;
+
 /**
  * simple redis distributed lock
  *
@@ -28,4 +33,52 @@ public class RedisLock {
         return EnumSingleton.INSTANCE.getSingleton();
     }
 
+    /** jedis poll */
+    private JedisPool jedisPool = new JedisPool("121.40.215.120", 6379);
+    /** expire time */
+    private static final String SET_WITH_EXPIRE_TIME = "EX";
+    /** set if not exist */
+    private static final String SET_IF_NOT_EXIST = "NX";
+
+    /**
+     * redis lock
+     *
+     * @param lockValue lock value
+     * @param seconds lock timeout
+     * @return is get the lock
+     */
+    public boolean lock(String lockValue, int seconds) {
+        Jedis jedis = jedisPool.getResource();
+        return "OK".equals(jedis.set(lockValue, lockValue, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, seconds));
+    }
+
+    /**
+     * use setNx to set lock
+     *
+     * @param lockValue lock value
+     * @param seconds lock timeout
+     * @return is get the lock
+     */
+    public boolean lock_setNx(String lockValue, int seconds) {
+        Jedis jedis = jedisPool.getResource();
+        Long result = jedis.setnx(lockValue, lockValue);
+        // while result = 1, lock success
+        if (result == 1L) {
+            return jedis.expire(lockValue, seconds) == 1L;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * release redis lock
+     *
+     * @param lock lock value
+     * @return is release redis lock
+     */
+    public boolean releaseLock(String lock) {
+        String luaSctipt = "if redis.call('get',KEYS[1]) == ARGV[1] then " + "return redis.call('del',KEYS[1]) else return 0 end";
+        Jedis jedis = jedisPool.getResource();
+        return jedis.eval(luaSctipt, Collections.singletonList(lock), Collections.singletonList(lock)).equals(1L);
+    }
 }
