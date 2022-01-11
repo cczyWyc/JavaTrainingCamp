12db284b698f36f463532ffa60d5fd6e6ab6dde3
cczyWyc <274230928@qq.com>
2022-01-12 01:23:33 +0800

    LockSupport demo

---
+++ week04_conc/src/main/java/com/cczyWyc/task/task_01/concurrent_02/lock/LockSupportDemo.java
@@ -0,0 +1,42 @@
+package com.cczyWyc.task.task_01.concurrent_02.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * LockSupport demo
+ *
+ * @author wangyc
+ */
+public class LockSupportDemo {
+    public static Object u = new Object();
+    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+
+    public static class ChangeObjectThread extends Thread {
+        public ChangeObjectThread(String name) {
+            super(name);
+        }
+        @Override
+        public void run() {
+            synchronized (u) {
+                System.out.println("in " + getName());
+                LockSupport.park();
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("The thread is interrupted");
+                }
+                System.out.println("The thread is running");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        Thread.sleep(1000L);
+        t2.start();
+        Thread.sleep(3000L);
+        t1.interrupt();
+        LockSupport.unpark(t2);
+        t1.join();
+        t2.join();
+    }
+}
